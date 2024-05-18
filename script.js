fcb3071568ee319c00df39a2e862c98f716a1752
Katie-Nam <134244734+Katie-Nam@users.noreply.github.com>
2024-05-17 22:36:04 -0400

    add all files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// var needHelpButton = $(".inNeedOfHelp");
+// var needHelpResources = $(".needHelpContainer");
+
+
+// needHelpButton.on("click", displayNeedHelpResouces);
+
+// function displayNeedHelpResouces() {
+//   needHelpResources.append(`
+//     <div class="needHelpResources">
+//       <p>Hi</p>
+//     </div>
+//   `);
+// }
